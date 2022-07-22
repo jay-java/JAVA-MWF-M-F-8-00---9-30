565d591683a630962b4a2df57e69419b633d0bda
jay-java <jayprajapati.tops@gmail.com>
2022-07-22 10:30:44 +0530

    final keyword updated

--- src/OOPS/FinalKeyword.java
+++ src/OOPS/FinalKeyword.java
@@ -2,19 +2,19 @@ package OOPS;
 //if variable is final then we cannot change value of varible
 // method ->cannot override
 //class -> cannot inherit
-final class Hardware1{
-	final int i = 10;
-	final public void keyboard() {
-		i++;
-		System.out.println("this is hrdware");
-	}
-}
-class PC1 extends Hardware1{
-	public void keyboard() {
-		System.out.println("this is pc");
-		super.keyboard();
-	}
-}
-public class FinalKeyword {
-
-}
+//final class Hardware1{
+//	final int i = 10;
+//	final public void keyboard() {
+//		i++;
+//		System.out.println("this is hrdware");
+//	}
+//}
+//class PC1 extends Hardware1{
+//	public void keyboard() {
+//		System.out.println("this is pc");
+//		super.keyboard();
+//	}
+//}
+//public class FinalKeyword {
+//
+//}
